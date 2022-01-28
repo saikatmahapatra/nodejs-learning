2e3ccf22561e314b0b8794aa9feea2998bf0b002
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-01-28 23:35:17 +0530

    file upload works

--- upload.js
+++ upload.js
@@ -2,19 +2,28 @@ const { Formidable } = require('formidable');
 var formidable = require('formidable');
 var http = require('http');
 var fs = require('fs');
+//var dir = require('dir');
 //create a upload form
 http.createServer(function(req, res){
 
     if(req.url == '/fileUpload'){
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files){
-            var oldpath = files.myFile.filepath;
-            res.write('file uploaded at ', oldpath);
-            res.end();
+            var oldPath = files.myFile.filepath;
+            var newPath = __dirname+'/uploads/'+files.myFile.originalFilename;
+            fs.rename(oldPath, newPath, function(err){
+                if (err) {
+                    throw err;
+                } else{
+                    res.write('file uploaded at '+oldPath+' ===> '+newPath);
+                    console.log('file uploaded at '+oldPath+' ===> '+newPath);
+                    res.end();
+                }
+            });
         });
     } else{
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileUpload" method="post" enctype="multipart/formdata">');
+        res.write('<form action="fileUpload" method="post" enctype="multipart/form-data">');
         res.write('<input type="file" name="myFile" id="myFile">');
         res.write('<button type="submit" class="btn btn-priamry">Upload File</button>');
         res.write('</form>');
