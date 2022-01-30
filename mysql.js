09afb0a5c20c17c082fa2a72aebdfb85559514e6
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2022-01-30 21:43:30 +0530

    mysql list record works

--- mysql.js
+++ mysql.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var mysql = require('mysql');
 var url = require('url');
+var isConnectedToDb = false;
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -18,14 +19,48 @@ http.createServer(function(req, res){
         database: 'db_nodejs_tut'
     });
 
-    conn.connect(function(err){
-        if(err) {
-            console.log(err);
-            throw err;
-        } else {
-            console.log('connected to mysql server');
-        }
-    });
+    if(!isConnectedToDb) {
+        conn.connect(function(err){
+            if(err) {
+                isConnectedToDb = false;
+                console.log(err);
+                throw err;
+            } else {
+                isConnectedToDb = true;
+                console.log('connected to mysql server');
+            }
+        });
+    }
+    
+
+    // Add users
+    if(isConnectedToDb && uriSegment.href == '/add') {
+        res.write('<h2>Add User</h2>');
+    }
+
+    // List users
+    if(isConnectedToDb && uriSegment.href == '/view') {
+        res.write('<h2>List User</h2>');
+        var sql = 'select * from users';
+        conn.query(sql, function(err, result, fields){
+            if(err) {
+                console.log(err);
+                throw err;
+            } else {
+                //console.log(result);
+                if(result && result.length> 0){
+                    //res.write('<table style="width: 100%; border: 1px #000 solid;">');
+
+                    result.forEach(element => {
+                        console.log(element.id, element.user_name, element.user_email);
+                    });
+                    //res.write('</table>');
+                } else {
+                    res.write('Sorry, no data fetched');
+                }
+            }
+        });
+    }
 
     res.end();
 }).listen(3000);
